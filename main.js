b59cb1f6ee43c639f6bc0df5cc09a9d3f4bf83d1
romatorres <romatorres12@gmail.com>
2024-11-06 16:47:39 -0300

    alterações na altura do header

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute("href"));
         if (target) {
-          const headerOffset = 80;
+          const headerOffset = 160;
           const elementPosition = target.getBoundingClientRect().top;
           const offsetPosition =
             elementPosition + window.pageYOffset - headerOffset;
