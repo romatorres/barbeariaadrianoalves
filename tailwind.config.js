21f7f4eb0a6cecaeaa13d26c9d450a4bb0af11a0
romatorres <romatorres12@gmail.com>
2024-11-06 22:01:42 -0300

    Alterções na estrutura de todas a seções

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./*.{js,ts,jsx,tsx}",],
-    theme: {
-      extend: {
-        fontFamily: {
-          primary: ['Poller One', 'cursive'],
-          secondary: ['Ubuntu', 'sans-serif'],
+  content: ["./index.html", "./*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Poller One", "cursive"],
+        secondary: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        primary: "#E2C244",
+        secondary: "#F5EBC3",
+        background: "#0E0D05",
+        black_secondary: "#171407",
+        gray: {
+          "01": "#26272B",
+          "02": "#4E525B",
+          "03": "#838896",
         },
-        colors: {
-          primary: '#E2C244',
-          secondary: '#F5EBC3',
-          background: '#0E0D05',
-          'black-secondary': '#1A1B1F',
-          gray: {
-            '01': '#26272B',
-            '02': '#4E525B',
-            '03': '#838896',
-          },
-          white: '#FAF3DC',
-        },
+        white: "#FAF3DC",
       },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-}
-
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
